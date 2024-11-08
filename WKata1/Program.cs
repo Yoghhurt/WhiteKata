73a8ba6631be95e513b6f18fa3f437365add0362
JackBillingham <billingham.jacks@gmail.com>
2024-11-08 16:28:00 +0100

    k2

--- WKata1/Program.cs
+++ WKata1/Program.cs
@@ -26,3 +26,23 @@ class Program
         }
     }
 }
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string characterName = "Arin";
+        int healthPoints = 100;
+        float attackPower = 15.5f;
+        bool isParalyzed = true;
+        double currencyAmount = 24.75;
+        
+        Console.WriteLine($"Character Name: {characterName}");
+        Console.WriteLine($"Health Points: {healthPoints}");
+        Console.WriteLine($"Attack Power: {attackPower}");
+        Console.WriteLine($"Is Paralyzed: {isParalyzed}");
+        Console.WriteLine($"Gold Coins: {currencyAmount}");
+    }
+}
